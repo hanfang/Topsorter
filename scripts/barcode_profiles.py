0414af6712bea4804f8e2701434d864df116cf3f
srividya <srividya.ramki@gmail.com>
2017-01-27 16:52:27 -0500

    merged samtools extract and index steps into the same python script

--- scripts/barcode_profiles.py
+++ scripts/barcode_profiles.py
@@ -12,7 +12,7 @@ import shutil
 import pysam
 import subprocess
 import argparse
-import pandas
+import pandas as pd
 import numpy
 
 def runProgram(logger=None,
@@ -35,6 +35,9 @@ def runProgram(logger=None,
 		progPath = progName
         # Construct shell command
         cmdStr = "%s %s" % (progPath,argStr)
+	logger.info("Processing Command : {0}".format(cmdStr))
+	start = time.time()
+
         # Set up process obj
         process = subprocess.Popen(cmdStr,
                                shell=True,
@@ -48,23 +51,38 @@ def runProgram(logger=None,
             logger.info(result)
         if logger is not None and stderr is not None and len(stderr) > 0:
             logger.info(stderr)
+	elasped=time.time()
         # Check returncode for success/failure
         if process.returncode != 0:
                 raise RuntimeError('Return Code : {0} , result {1} , progName {2}'.format(process.returncode,result,progName))
         # Return result
-        return { "result" : result , "stderr" :stderr }
+        return { "result" : result , "stderr" :stderr, "exec_time" :' %.2fs' % (elasped - start) }
 
 def getReadsinRegion(logger,bam_file,bed_file,output_file):
  """
   Get the Reads in the regions of bed file
  """ 
- cmd = "view -o {0} -L {1} {2}".format(output_file,bam_file,bed_file)
+ cmd = "view -o {0} -hb -L {1} {2}".format(output_file,bed_file,bam_file)
+ 
  try:
- 	runProgram(logger,"samtools",cmd,None,os.getcwd())
+ 	res = runProgram(logger,"samtools",cmd,None,os.getcwd())
+	logger.info("Extracting VCF regions from Alignment completed in {0}".format(res['exec_time']))
  except Exception, e:
 	raise Exception("".join(traceback.format_exc()))
 
 
+def buildSamIndex(logger,bam_file):
+ """
+  Build samtools index for the bam file
+ """ 
+ cmd = "index {0}".format(bam_file)
+ print cmd
+ try:
+	dir_name=os.path.dirname(bam_file)
+	res = runProgram(logger,"samtools",cmd,None,os.getcwd())
+	logger.info("Building Samtools index for overlapping Alignments completed in {0}".format(res['exec_time']))
+ except Exception, e:
+	raise Exception("".join(traceback.format_exc()))
 
 def buildcov(logging,bam_file,bed_file,output_bed):
  """
@@ -93,8 +111,51 @@ def buildcov(logging,bam_file,bed_file,output_bed):
  samfile.close()
  output_bed.close()
  after = time.time()
- print 'completed in %.2fs' % (after - before)
- pass
+ logger.info('Computed Barcode profiles for the vcf regions completed in %.2fs' % (after - before))
+
+def count_barcode_overlaps(logging,bed_file,tenkb_bed):
+ """
+   Count barcode overlap between the network paths and write it to a txt file 
+ """
+ before = time.time()
+ ###
+ tenkb_bed = open(output_bed,"wb")
+ tenkb_bed.close()
+ after = time.time()
+ logging,info('Counting barcode overlaps in the vcf regions completed in %.2fs' % (after - before))
+
+def count_barcode_overlaps(logging,bed_file,output_bed):
+ """
+   Count barcode overlap between the network paths and write it to a txt file 
+ """
+ before = time.time()
+ ###
+ output_bed = open(output_bed,"wb")
+ data = pd.read_table(bed_file, header = None)
+ #print data.head(10)
+ v_infos = set(data[3])
+ u_infos= set([ v.split(",")[0] for v in list(v_infos)])
+ #print len(v_infos)
+ #print len(u_infos)
+ #print u_infos
+ output_bed.write("Variant_Info\tbefore->left\tbefore->right\tbefore->after\tleft->right\tleft->after\tright->after\n")
+ for u in u_infos:
+        print u+",before"
+        print u+',after'
+        before = data.loc[data[3] == u+",before"]
+        after = data.loc[data[3] == u+',after']
+        left =  data.loc[data[3] == u+',left']
+        right = data.loc[data[3] == u+',right']
+        b_to_a = set(before[4]) & set(after[4])
+        b_to_l = set(before[4]) & set(left[4])
+        b_to_r = set(before[4]) & set(right[4])
+        l_to_r = set(left[4]) & set(right[4])
+        l_to_a = set(left[4]) & set(after[4])
+        r_to_a = set(right[4]) & set(after[4])
+        output_bed.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(u,str(len(b_to_a)),str(len(b_to_l)),str(len(b_to_r)),str(len(l_to_r)),str(len(l_to_a)),str(len(r_to_a))))
+ output_bed.close()
+ after = time.time()
+ logging,info('Counting barcode overlaps in the vcf regions completed in %.2fs' % (after - before))
 
 if __name__ == "__main__":
     logger = logging.getLogger('barcode_profiling')
@@ -107,13 +168,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-bam",dest="bam_file",help="Barcoded Bam alignment file from 10x data")
     parser.add_argument("-bed",dest="bed_file",help="Bed file for the regions near variants")
-    parser.add_argument("-o",dest="output",help="output file name")
+    parser.add_argument("-o",dest="output",help="output dir name")
     args = parser.parse_args() 
     bam_file = args.bam_file
     bed_file = args.bed_file
-    #if not os.path.exists(args.output_dir) : os.mkdir(args.output_dir)
-    #output_file = os.path.join(args.output_dir,"overlapping_reads.sam")
-    #getReadsinRegion(logger,args.bam_file,args.bed_file,output_file)
+    if not os.path.exists(args.output) : os.mkdir(args.output)
+    output_file = os.path.join(args.output,"overlapping_reads.bam")
+    getReadsinRegion(logger,args.bam_file,args.bed_file,output_file)
+    buildSamIndex(logger,output_file)
     #if not os.path.exists(output_file):
     #	raise Exception("{0} Not Found".format(output_file))
-    buildcov(logger,bam_file,bed_file,args.output) 
+    barcode_file = os.path.join(args.output,"reads_barcode_profile.txt")
+    buildcov(logger,bam_file,bed_file,barcode_file)
+    #tenkbfile="/seq/schatz/sramakri/NA24385_GRCh37/NA24385_GRCh37.bam.manta.vcf.chr20_shit_new.bed" 
+    #overlap_file=os.path.join(args.output,"barcode_overlaps_between_regions.txt")
+    #count_barcode_overlaps(logger,tenkbfile,overlap_file)
